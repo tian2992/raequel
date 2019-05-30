0760a40bdaa38a79b53930c4dc9e73f2b12b3346
Sebastian Oliva <code@sebastianoliva.com>
2019-05-29 18:07:57 -0600

    nowify

---
+++ now.js
@@ -0,0 +1,9 @@
+{
+  "name": "raequel"
+  "version": 2,
+  "name": "python-flask",
+  "builds": [
+    { "src": "**/*.py", "use": "@now/python@canary" },
+    { "src": "css/**", "use": "@now/static" }
+  ]
+}
